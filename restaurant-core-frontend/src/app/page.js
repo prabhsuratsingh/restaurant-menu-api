59bb8d1834e2a75df8d2d323a5725852cda23c01
prabhsuratsingh <prabhsurat17@gmail.com>
2024-09-22 22:52:19 +0530

    created home page

--- restaurant-core-frontend/src/app/page.js
+++ restaurant-core-frontend/src/app/page.js
@@ -1,95 +1,124 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client";
 
-export default function Home() {
+import { useEffect, useState } from "react";
+import { useRouter, useSearchParams } from "next/navigation";
+
+async function deleteMenu(id) {
+  const res = await fetch(`http://127.0.0.1:3000/api/menu/${id}/`, {
+    method: "DELETE",
+  });
+
+  if(!res.ok) {
+    throw new Error("Failed to retrieve menu!");
+  }
+
+  return Promise.resolve(); 
+}
+
+async function getData() {
+  const res = await fetch("http://127.0.0.1:3000/api/menu/");
+  if(!res.ok) {
+    throw new Error("Failed to fetch data!");
+  }
+
+  return res.json();
+}
+
+
+const MenuItem = ({ id, name, price, onEdit, onDelete }) => {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div className="menu-item" data-id={id}>
+      <div className="menu-item-info">
+        <div className="menu-item-name">{name}</div>
+        <div className="menu-item-price">${price.toFixed(2)}</div>
+      </div>
+      <div className="menu-item-actions">
+        <button className="edit-button" onClick={onEdit}>
+          Edit
+        </button>
+        <button 
+          className="delete-button"
+          onClick={() => {
+            deleteMenu(id).then(() => onDelete(id));
+          }}>
+          Delete
+        </button>
+      </div>
+    </div>
+  );
+}
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
+
+export default function Page() {
+  const [menuItems, setMenuItems] = useState(null);
+  const router = useRouter();
+  const params = useSearchParams();
+
+  //state for displaying success message
+  const [displaySuccessMessage, setDisplaySuccessMessage] = useState({
+    show: false,
+    type: "",
+  });
+
+  //fetch menu items on component mount
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await getData();
+      setMenuItems(data);
+    };
+    fetchData().catch(console.error);
+  }, []);
+
+  //detect changes url params for success message
+  useEffect(() => {
+    if(!!params.get("action")) {
+      setDisplaySuccessMessage({
+        type: params.get("action"),
+        show: true,
+      });
+      router.replace("/");
+    }
+  }, [params, router]);
+
+  //automatically hide the success message after 3 seconds
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      if(displaySuccessMessage.show) {
+        setDisplaySuccessMessage({ show:false, type:""});
+      }
+    }, 3000);
+    return () => clearTimeout(timer);
+  }, [displaySuccessMessage.show]);
+
+  //handle deletion of menu items
+  const handleDelete = (id) => {
+    setMenuItems((items) => items.filter((item) => item.id !== id));
+  };
+
+  return (
+    <div>
+      <button className="add-button" onClick={() => router.push("/add")}>
+        Add
+      </button>
+      {displaySuccessMessage.show && (
+        <p className="success-message">
+          {displaySuccessMessage.type === "add" ? "Added a" : "Modified a"} menu item
+        </p>
+      )}
+      {menuItems ? (
+        menuItems.map((item) => (
+          <MenuItem
+            key={item.id}
+            id={item.id}
+            name={item.name}
+            price={item.price}
+            onEdit={() => router.push(`/update/${item.id}`)}
+            onDelete={handleDelete}
           />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        ))
+      ) : (
+        <p>Loading....</p>
+      )}
     </div>
   );
 }
