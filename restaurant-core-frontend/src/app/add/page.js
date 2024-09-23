c406f38e6fc840ca634a106a67d07dc06b47c5b5
prabhsuratsingh <prabhsurat17@gmail.com>
2024-09-23 21:50:03 +0530

    Created add menu item page

---
+++ restaurant-core-frontend/src/app/add/page.js
@@ -0,0 +1,81 @@
+"use client"
+
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+
+async function createMenu(data) {
+  const res = await fetch("http://127.0.0.1:3000/api/menu/", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data)
+  });
+
+  if(!res.ok) {
+    throw new Error("Failed to create data!"); 
+  }
+
+  return res.json();
+}
+
+
+const Page = () => {
+  const router = useRouter();
+  const [formData, setFormData] = useState({name: "", price: ""});
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const onFinish = (event) => {
+    event.preventDefault();
+    setIsLoading(true);
+    createMenu(formData)
+      .then(() => {
+        router.replace("/?action=add");
+      })
+      .catch(() => {
+        setError("An Error occured!");
+        setIsLoading(false);
+      });
+  };
+
+  useEffect(() => {
+    return () => setIsLoading(false)
+  }, []);
+
+  return (
+    <form onSubmit={onFinish}>
+      <div className="form-item">
+        <label htmlFor="name">Name</label>
+        <input
+          required
+          name="name"
+          value={formData.name}
+          onChange={(event) =>
+            setFormData({...formData, name: event.target.value})
+          }
+        />
+      </div>
+      <div className="form-item">
+        <label htmlFor="price">Price</label>
+        <input
+          required
+          type="number"
+          name="price"
+          value={formData.price}
+          onChange={(event) =>
+            setFormData({...formData, price: event.target.value})
+          }
+        />
+      </div>
+      {error && <p className="error-message">{error}</p>}
+      <div>
+        <button disabled={isLoading} className="add-button" type="submit">
+          Submit
+        </button>
+      </div>
+    </form>
+  );
+};
+
+export default Page;
